0bec40b01cfdd667f80e190db22c7763d7be68bc
Sergio Núñez <sergio-n-g@hotmail.com>
2019-05-22 18:09:58 -0600

    Added the Board parent component

--- src/Note.js
+++ src/Note.js
@@ -1,32 +1,65 @@
 import React, { Component } from 'react';
-import { FaEdit, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash, FaSave } from 'react-icons/fa';
 
 class Note extends Component {
     constructor (props) {
         super(props);
+        this.state = {
+            editing: false
+        }
         this.edit = this.edit.bind(this);
         this.remove = this.remove.bind(this);
+        this.save = this.save.bind(this);
+        this.renderDisplay = this.renderDisplay.bind(this);
+        this.renderForm = this.renderForm.bind(this);
     }
 
     edit() {
-        alert('editing note');
-    };
+        //alert('editing note');
+        this.setState({
+            editing: true
+        });
+    }
 
     remove() {
         alert('removing note');
-    };
+    }
 
-    render() {
+    save() {
+        alert(this._newText.value);
+    }
+
+    renderForm() {
         return (
             <div className="note">
-                <p>Learn React</p>
+                <form>
+                    <textarea ref={ input => this._newText = input } />
+                    <button onClick={this.save}><FaSave /></button>
+                </form>
+            </div>
+        );
+    }
+
+    renderDisplay() {
+        return (
+            <div className="note">
+                <p>{this.props.children}</p>
                 <span>
                     <button id="edit" onClick={this.edit} ><FaEdit /></button>
                     <button id="remove" onClick={this.remove} ><FaTrash /></button>
                 </span>
             </div>
         );
-    };
-};
+    }
+
+    render () {
+        return this.state.editing ? this.renderForm() : this.renderDisplay();
+        /* if (this.state.editing){
+            return this.renderForm();
+        } else {
+            return this.renderDisplay();
+        } */
+    }
+}
 
 export default Note;
