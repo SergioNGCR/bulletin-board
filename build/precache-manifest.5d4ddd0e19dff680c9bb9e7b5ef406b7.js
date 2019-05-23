46ca9e711369b3ba6fe3c7539b298c4079fc0620
SergioNGCR <sergio-n-g@hotmail.com>
2019-05-23 01:53:39 -0600

    Getting things ready for heroku publishing

---
+++ build/precache-manifest.5d4ddd0e19dff680c9bb9e7b5ef406b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358119e748d021720b8864f9c09433a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36fbd4462a0cd731bc5c",
+    "url": "/static/css/main.1aa113e1.chunk.css"
+  },
+  {
+    "revision": "a9a6658923ed72c3993e",
+    "url": "/static/js/2.d5aca68e.chunk.js"
+  },
+  {
+    "revision": "36fbd4462a0cd731bc5c",
+    "url": "/static/js/main.81b01023.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
